4426ff131f9b67317d58877950eb8c0d7709397f
jmlemoine <jeanmelvinlp27@gmail.com>
2019-10-09 17:07:22 -0400

    Agregando algunas dependencias

--- src/main/java/CRUD/Main.java
+++ src/main/java/CRUD/Main.java
@@ -1,12 +1,37 @@
 package CRUD;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.Version;
 import spark.Spark;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.staticFiles;
 
 public class Main {
 
     public static void main(String[] args){
 
+        staticFiles.location("/Templates");
+        final Configuration config = new Configuration(new Version(2, 3, 0));
+        config.setClassForTemplateLoading(Main.class, "/");
+
+        ArrayList<Estudiante> listEstudiantes = new ArrayList<Estudiante>();
+        Spark.get("/", ((request, response) -> {
+            Template plantillaInicio = config.getTemplate("Templates/Inicio.ftl");
+            StringWriter writer = new StringWriter();
+
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("listEstudiantes", listEstudiantes);
+            attributes.put("tamanoLista", listEstudiantes.size() > 0);
+            plantillaInicio.process(attributes, writer);
+            return writer;
+
+        }));
 
 
     }
