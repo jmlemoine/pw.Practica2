d128d8009ec5a6f32d58666d32644982ef85dfd9
jmlemoine <jeanmelvinlp27@gmail.com>
2019-10-09 16:29:05 -0400

    Creando el constructor de la clase Estudiante

--- src/main/java/CRUD/Estudiante.java
+++ src/main/java/CRUD/Estudiante.java
@@ -7,4 +7,12 @@ public class Estudiante {
     String apellido;
     String telefono;
 
+    public Estudiante(int matricula, String nombre, String apellido, String telefono){
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+
+    }
+
 }
